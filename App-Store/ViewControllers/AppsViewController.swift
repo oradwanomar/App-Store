b5066e6e36fb3e12acea9aad21bc9cd5f72d9c69
omarradwan <oradwan037@gmail.com>
2022-06-12 07:17:02 +0200

    First Commit

---
+++ App-Store/ViewControllers/AppsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App-Store
+//
+//  Created by Omar Ahmed on 12/06/2022.
+//
+
+import UIKit
+
+class AppsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
